35342521de66374115cba07905091b298eaa047d
Juanjoseve3 <jamalapatruca@gmail.com>
2022-05-10 18:10:45 -0500

    Se agrega el proyecto

---
+++ StringCalculatorTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("StringCalculatorTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringCalculatorTest")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("398be353-180f-4b4b-b476-54e91e8c5208")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ StringCalculatorTest/StringCalculatorShould.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace StringCalculatorTest
+{
+    [TestClass]
+    public class StringCalculatorShould
+    {
+        [TestMethod]
+        public void Return_0_when_Receives_Empty()
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Add("");
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void Return_Number_when_Receives_That_Number()
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Add("1");
+            Assert.AreEqual(1, result);
+        }
+
+        [TestMethod]
+        public void Return_Sum_When_Receives_Two_Numbers()
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Add("1,2");
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void Return_Sum_When_Receives_Any_Amount_of_Numbers()
+        {
+            var calculator = new StringCalculator();
+            var result = calculator.Add("1,2,3,4");
+            Assert.AreEqual(10, result);
+        }
+    }
+}
